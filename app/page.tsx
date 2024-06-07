0bd1a9186405e39d6cc70c34647e5d72ca17e09b
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2024-06-07 19:17:59 +0300

    Start Demo

--- app/page.tsx
+++ app/page.tsx
@@ -10,13 +10,11 @@ const rotations = [-4, -2, 0, 2, 4]
 //rotations[rotations.indexOf(rotate.get()) > 0 ? rotations.indexOf(rotate.get()) + 1 : rotations.indexOf(rotate.get())]
 export default function Home() {
 	const [scrolled, setScrolled] = useState(false)
-	const [rotate, setRotate] = useState(-4)
-	const [rotatePrev, setRotatePrev] = useState(-4)
 	const [image, setImage] = useState('iphoneFirst.png')
 	const [title, setTitle] = useState('Financial Services')
 	const [text, setText] = useState('Access financial services such as borrowing loans, take a credit card, and opening a bank account.')
 	const [textAnimation, setTextAnimation] = useState('enter')
-	const [animating, setAnimating] = useState(true)
+	const [demoShown, setDemoShown] = useState(false)
 
 	const willChange = useWillChange()
 
@@ -25,13 +23,6 @@ export default function Home() {
 	const secondTargetRef = useRef<HTMLDivElement>(null)
 
 	const mainSectionInView = useInView(mainSection, { once: false })
-
-	useEffect(() => {
-		console.log(mainSectionInView)
-		if(mainSectionInView) {
-			setAnimating(true)
-		}
-	}, [mainSectionInView])
 
 	useEffect(() => {
 		const lenis = new Lenis({
@@ -84,6 +75,10 @@ export default function Home() {
 	// 	// else cycleRotate()
 	// })
 
+	useEffect(() => {
+		console.log(demoShown)
+	}, [demoShown])
+
 	const rotateVal = useTransform(scrollYProgress, [0.15, 0.20, 0.30, 0.35, 0.40], [-4, -2, 0, 2, 4])
 
 	const delay = useTransform(scrollYProgress, (pos) => {
@@ -113,6 +108,7 @@ export default function Home() {
 		else if(pos <= 0.30) return 'iphoneThird.png'
 		else if(pos <= 0.35) return 'iphoneFourth.png'
 		else if(pos <= 0.40) return 'iphoneFifth.png'
+		else return 'showDemo'
 	})
 
 	const titleVal = useTransform(scrollYProgress, (pos) => {
@@ -140,11 +136,20 @@ export default function Home() {
     });
 
 	useMotionValueEvent(imageVal, 'change', (pos) => {
-		setImage(pos!)
-		setTextAnimation('exit')
-		setTimeout(() => {
-			setTextAnimation('enter')
-		}, 150)
+		console.log(pos)
+		if(pos === 'showDemo') 
+		{
+			setDemoShown(true)
+		}
+		else 
+		{
+			if(demoShown) setDemoShown(false)
+			setImage(pos!)
+			setTextAnimation('exit')
+			setTimeout(() => {
+				setTextAnimation('enter')
+			}, 150)
+		}
 	})
 
 	useMotionValueEvent(titleVal, 'change', (pos) => {
@@ -166,7 +171,7 @@ export default function Home() {
 		<main ref={targetRef} className={cn("flex flex-col px-12 pt-6 no-scroll-bar min-h-[600vh]")}>
 			<AnimatePresence mode='popLayout'>
 				{!scrolled && (
-					<motion.section exit={{ y: "-100vh" }} transition={{ duration: 0.5 }} key="firstHero" className='flex flex-col max-h-screen flex-1'>
+					<motion.section exit={{ y: "-100vh" }} transition={{ duration: 0.5 }} key="firstHero" className='flex flex-col max-h-screen flex-1 z-30'>
 						<Header />
 						<section className='relative rounded-t-3xl flex flex-col pt-20 px-4 items-center gap-8 flex-1'>
 							<Image
@@ -205,18 +210,14 @@ export default function Home() {
 								initial={{ left: 'calc(50% - 200px)' }}
 								exit={{ rotate: -4, position: 'fixed', bottom: '-550px', y: 'calc(50vh - 228px)', left: 'calc(36.2% - 150px)', opacity: 0.65, width: '300px', height: '614px'}}
 								transition={{ duration: 0.40, ease: 'easeInOut' }}
-								onAnimationComplete={() => setTimeout(() => {
-									setAnimating(false)
-								}, 500)}
-								onAnimationStart={() => setAnimating(true)}
 								className='fixed -bottom-[550px] w-[400px] h-[818px]'
 								key='phone'
 							/>
 						</section>
 					</motion.section>
 				)}
-				{scrolled && (
-					<motion.section ref={secondTargetRef} transition={{ duration: 0 }} key="secondHero" className={cn('flex sticky top-0 max-h-screen flex-1 items-center justify-center pl-[15%]')}>
+				{scrolled && !demoShown && (
+					<motion.section ref={secondTargetRef} transition={{ duration: 0 }} key="secondHero" className={cn('flex sticky top-0 max-h-screen flex-1 items-center justify-center pl-[15%] z-20')}>
 						<MotionImage
 							src='/images/triangleBg.svg'
 							width={782}
@@ -240,10 +241,12 @@ export default function Home() {
 									//@ts-expect-error rotate
 									style={{ rotate: rotateVal, willChange }}
 									transition={{ duration: duration.get(), ease: 'easeInOut', delay: delay.get() ?? 0.5 }}
+									exit={scrolled ? { opacity: 0 } : undefined}
 									className='max-h-[614px] max-w-[300px]'
 									key='phone'
 									viewport={{ once: true }}
 									priority
+									layoutId={scrolled ? 'phone' : undefined}
 								/>
 							</div>
 							<motion.div className='flex flex-1 flex-col gap-4 overflow-hidden'>
@@ -251,6 +254,35 @@ export default function Home() {
 								<motion.p variants={{ enter: { opacity: [0, 1], y: ['300px', '0px'] }, exit: { opacity: [0, 1], y: ['0px', '-300px'] }}} animate={textAnimation} transition={{ duration: 0.3, ease: 'easeInOut', delay: delay.get() ?? 0.5 }} className='font-semibold max-w-[380px] text-[32px] leading-[2.75rem]'>{text}</motion.p>
 							</motion.div>
 						{/* </div> */}
+					</motion.section>
+				)}
+				{demoShown && (
+					<motion.section initial={{ y: '100vh' }} animate={{ y: '0' }} transition={{ duration: 0.75 }} key='demo' className='flex sticky top-0 min-h-screen flex-col items-center justify-center gap-4 z-10'>
+						<div className='relative flex flex-col items-center justify-center gap-4 bg-[#F1E8E6] w-full pt-20 pb-16 rounded-3xl overflow-hidden'>
+							<MotionImage
+								src='/images/demoTriangle.png'
+								width={782}
+								height={1113}
+								alt='Dubai'
+								className='absolute z-[11] bottom-0'
+								initial={bgInitial.get()}
+								animate={{ opacity: 1}}
+								transition={{ duration: 0, delay: 0.15 }}
+								viewport={{ once: true }}
+							/>
+							<MotionImage
+								src={`/images/${image}`}
+								alt='Phone'
+								width={300}
+								height={614}
+								className='max-h-[614px] max-w-[300px] z-[12] mt-8'
+								key='phone'
+								layoutId='phone'
+								initial={{ opacity: 0 }}
+								animate={{ opacity: 1 }}
+								transition={{ duration: 0.5, delay: 0.5, ease: 'easeInOut' }}
+							/>
+						</div>
 					</motion.section>
 				)}
 			</AnimatePresence>
