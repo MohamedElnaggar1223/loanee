150fe7536ad0ed807e27f4e55d170fde7bdf17e1
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2024-06-10 00:23:20 +0300

    Mobile View

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const figtree = Figtree({ subsets: ["latin"] });
 
@@ -9,6 +10,32 @@ export const metadata: Metadata = {
   description: "Your personal finance assistant",
 };
 
+const demoImagesRotation = [
+	'iphoneSecond.png',
+	'demoIphoneTwelvth.png',
+	'demoIphoneThirteenth.png',
+	'demoIphoneFirst.png',
+	'demoIphoneSecond.png',
+	'demoIphoneThird.png',
+	'demoIphoneFourth.png',
+	'demoIphoneFifth.png',
+	'demoIphoneSixth.png',
+	'demoIphoneSeventh.png',
+	'demoIphoneEighth.png',
+	'demoIphoneNinth.png',
+	'demoIphoneTenth.png',
+	'demoIphoneEleventh.png',
+]
+
+const FeatureImages = [
+    'iphoneHero.png',
+    'iphoneFirst.png',
+    'iphoneSecond.png',
+    'iphoneThird.png',
+    'iphoneFourth.png',
+    'iphoneFifth.png',
+]
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -16,6 +43,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        {demoImagesRotation.map((image) => (
+          <link key={image} rel="preload" href={`/images/${image}`} as="image" />
+        ))}
+        {FeatureImages.map((image) => (
+          <link key={image} rel="preload" href={`/images/${image}`} as="image" />
+        ))}
+      </Head>
       <body className={figtree.className}>{children}</body>
     </html>
   );
