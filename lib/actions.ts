27f664ebe5b82eeb24e84a6c1e3989f049c902a3
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2024-06-27 15:46:10 +0300

    Emails

--- lib/actions.ts
+++ lib/actions.ts
@@ -8,13 +8,14 @@ const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_KEY!)
 export const sendMail = (data: z.infer<typeof signUpSchema>) => {
     try
     {
+        console.log('entered')
         resend.emails.send({
             from: 'onboarding@resend.dev',
             to: 'hello@loanee.ae',
             // to: 'maelnaggar1223@gmail.com',
             subject: 'New sign up',
             html: `<p><strong>Email:</strong> ${data.email} <strong>First name:</strong> ${data.fistName} <strong>Number:</strong> ${data.countryCode}${data.mobile} <strong>Job title:</strong> ${data.jobTitle}</p>`
-        })
+        }).then(data => console.log(data.data?.id))
     }
     catch (error)
     {
